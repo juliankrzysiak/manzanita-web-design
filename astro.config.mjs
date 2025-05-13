a44d1a2c8090019794749175fd9ae97c06563b44
Julian Krzysiak <jkrzysiak13@gmail.com>
2025-05-12 23:32:50 -0700

    Add gsap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,26 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
+  experimental: {
+    fonts: [
+      {
+        provider: fontProviders.google(),
+        name: "Proza Libre",
+        cssVariable: "--font-proza-libre",
+      },
+      {
+        provider: fontProviders.google(),
+        name: "Spectral",
+        cssVariable: "--font-spectral",
+        weights: ["300", "400"],
+      },
+    ],
+  },
 });
