00bd517a1338bb193a200bc623d5fbb2d02be0d6
Julian Krzysiak <jkrzysiak13@gmail.com>
2026-04-08 14:41:37 -0700

    Remove dev toolbar for project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-import { defineConfig, fontProviders } from "astro/config";
+import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 
@@ -7,5 +7,8 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
+  },
+  devToolbar: {
+    enabled: false,
   },
 });
